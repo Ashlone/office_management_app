3340d59385378b17d0358e589618b19a6ddc845e
Ashlone <ashchibvuri@gmail.com>
2022-08-28 20:28:49 +0200

    improved app

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,14 @@ import { getStorage } from "firebase/storage";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAZsju-SY0L-GLzrmHQhSM5jKdQCI6AMHM",
-  authDomain: "officeapp-6dcf7.firebaseapp.com",
-  projectId: "officeapp-6dcf7",
-  storageBucket: "officeapp-6dcf7.appspot.com",
-  messagingSenderId: "432297652147",
-  appId: "1:432297652147:web:3c4f44fc49c75c45ada5c6"
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
